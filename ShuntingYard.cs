f0cc3b153d330b01bb558a6f9333aa1453449f4f
Oprea Rares-Andrei <Rares.OPREA@lbs4.salzburg.at>
2025-10-21 11:30:42 +0200

    Update tokens & ShuntingYard

---
+++ ShuntingYard.cs
@@ -0,0 +1,45 @@
+﻿namespace Labor
+{
+    public class ShuntingYard
+    {
+        public static Queue<string> InfixToPostfix(IEnumerable<string> tokens, Func<string, int> getPrecedence, Func<string, bool> isLeftAssociative)
+        {
+            var outputQueue = new Queue<string>();
+            var operatorStack = new Stack<string>();
+            foreach (var token in tokens)
+            {
+                if (int.TryParse(token, out _))
+                {
+                    outputQueue.Enqueue(token);
+                }
+                else if (token == "(")
+                {
+                    operatorStack.Push(token);
+                }
+                else if (token == ")")
+                {
+                    while (operatorStack.Count > 0 && operatorStack.Peek() != "(")
+                    {
+                        outputQueue.Enqueue(operatorStack.Pop());
+                    }
+                    operatorStack.Pop();
+                }
+                else
+                {
+                    while (operatorStack.Count > 0 && operatorStack.Peek() != "(" &&
+                           (getPrecedence(operatorStack.Peek()) > getPrecedence(token) ||
+                            (getPrecedence(operatorStack.Peek()) == getPrecedence(token) && isLeftAssociative(token))))
+                    {
+                        outputQueue.Enqueue(operatorStack.Pop());
+                    }
+                    operatorStack.Push(token);
+                }
+            }
+            while (operatorStack.Count > 0)
+            {
+                outputQueue.Enqueue(operatorStack.Pop());
+            }
+            return outputQueue;
+        }
+    }
+}
--- Tokenizer.cs
+++ Tokenizer.cs
@@ -1,35 +1,69 @@
 ﻿using System.Text;
+using System.Globalization;
 
 namespace Labor
 {
-    internal class Tokenizer
+    public class Tokenizer
     {
         public static List<object>? Tokenize(string input)
         {
             if (string.IsNullOrWhiteSpace(input)) return null;
-            var tokens = new Stack();
-            var tokenList = tokens.GetElements();
+
+            var tokens = new List<object>();
             var currentToken = new StringBuilder();
+
             foreach (char c in input)
             {
                 if (char.IsWhiteSpace(c))
                 {
+                    // Das letzte Token
                     if (currentToken.Length > 0)
                     {
-                        tokens.Push(currentToken.ToString());
+                        tokens.Add(ParseToken(currentToken.ToString()));
                         currentToken.Clear();
                     }
                 }
-                else
+                else if (IsOperator(c) || c == '(' || c == ')')
+                {
+                    // Das letzte Zahl Token
+                    if (currentToken.Length > 0)
+                    {
+                        tokens.Add(ParseToken(currentToken.ToString()));
+                        currentToken.Clear();
+                    }
+
+                    tokens.Add(c.ToString());
+                }
+                else if (char.IsDigit(c) || c == '.')
                 {
                     currentToken.Append(c);
                 }
+                else
+                {
+                    throw new ArgumentException($"Ungültiges Zeichen im Ausdruck: {c}");
+                }
             }
+
+            // Das letzte Token addieren, falls vorhanden
             if (currentToken.Length > 0)
             {
-                tokens.Push(currentToken.ToString());
+                tokens.Add(ParseToken(currentToken.ToString()));
             }
-            return tokenList;
+
+            return tokens;
+        }
+
+        private static bool IsOperator(char c)
+        {
+            return c == '+' || c == '-' || c == '*' || c == '/';
+        }
+
+        private static object ParseToken(string token)
+        {
+            // Parsen der Zahl
+            if (double.TryParse(token, NumberStyles.Float, CultureInfo.InvariantCulture, out double number))
+                return number;
+            return token; // entweder Operator oder Klammer
         }
     }
 }
